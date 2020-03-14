c08241dbbfabba2a64f3d79ca96cbb553c7db581
Vishal Chavan <vishalchavanvicky@gmail.com>
2020-03-15 00:04:56 +0530

    Layout Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    NavComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
