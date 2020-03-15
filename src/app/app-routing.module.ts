b93453a562807114a5c3f2a221a81e6263890005
Vishal Chavan <vishalchavanvicky@gmail.com>
2020-03-15 13:13:04 +0530

    header completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'Login', component: LoginComponent },
 ];
 
 @NgModule({
