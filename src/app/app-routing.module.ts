0350272a08a062e46036ed25e86a0e72a4629440
Kaustubh Jadhav <jadhav.kaustubh@gmail.com>
2020-03-16 15:55:58 +0530

    adds login as front page and then home page is displayed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'Login', component: LoginComponent },
+  { path: '', component: LoginComponent },
+  { path: 'home', component:  HomeComponent},
 ];
 
 @NgModule({
