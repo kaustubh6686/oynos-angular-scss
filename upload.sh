5dceea0482285ffb02f170b92e558e35e0f9ee76
Kaustubh Jadhav <jadhav.kaustubh@gmail.com>
2020-03-16 13:54:43 +0530

    adds upload script

---
+++ upload.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+HOST='ftp.g-tek.tech'
+USER='g-tek.tech'
+PASS='54d6d250148c'
+#TARGETFOLDER='/From-Scratch'
+TARGETFOLDER='/public_html'
+SOURCEFOLDER="/home/kaustubh/Desktop/PROJECTS/oynos-scss/dist/oynos-scss"
+
+lftp -f "
+set ftp:ssl-allow no
+open $HOST
+user $USER $PASS
+lcd $SOURCEFOLDER
+mirror --reverse --delete --verbose $SOURCEFOLDER $TARGETFOLDER
+bye
+"
